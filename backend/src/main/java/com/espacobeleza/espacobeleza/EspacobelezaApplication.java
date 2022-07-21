874ed0fc9e69644473ee842f4a7e4ce92efb4785
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-07-21 18:00:30 -0300

    Projeto criado

---
+++ backend/src/main/java/com/espacobeleza/espacobeleza/EspacobelezaApplication.java
@@ -0,0 +1,13 @@
+package com.espacobeleza.espacobeleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacobelezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacobelezaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/espacobeleza/espacobeleza/EspacobelezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espacobeleza.espacobeleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacobelezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
